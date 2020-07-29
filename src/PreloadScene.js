f3f212e430f183c6af19b02d469f575901989c63
djordjega <djordjemgavrilovic@yahoo.co.uk>
2020-07-29 20:16:27 +0200

    preload scene menu update

--- src/PreloadScene.js
+++ src/PreloadScene.js
@@ -24,9 +24,26 @@ class PreloadScene extends Scene {
     create() {
         const txt = this.add.text(400,300,'.hello.sinclair', { fontSize:'50px', fill:'#ff0000' });
         txt.setOrigin(0.5);
-        this.input.on('pointerdown', () => {
-            this.scene.start('level1');
-        });
+        // this.input.on('pointerdown', () => {
+        //     this.scene.start('level3');
+        // });
+        
+        const lvl1 = this.add.text(200,400, 'level1', { fontSize: '20px', fill: '#b60000'});
+        lvl1.setInteractive();
+        lvl1.on('pointerdown', () => { this.scene.start('level1') });
+        const lvl2 = this.add.text(200,420, 'level2', { fontSize: '20px', fill: '#b60000'});
+        lvl2.setInteractive();
+        lvl2.on('pointerdown', () => { this.scene.start('level2') });
+        const lvl3 = this.add.text(200,440, 'level3', { fontSize: '20px', fill: '#b60000'});
+        lvl3.setInteractive();
+        lvl3.on('pointerdown', () => { this.scene.start('level3') });
+
+        const demo_txt = this.add.text(200,600,'platformer demo project', {fontSize: '12px', fill: '#ff0000'});
+
+        const ivoli_link = this.add.text(600,600,'ivoli.net', {fontSize: '12px', fill: '#ff0000'});
+        ivoli_link.setInteractive();
+        ivoli_link.on('pointerdown', () => { window.location.href='http://ivoli.net' });
+
     }
 
     // ========================================================================
